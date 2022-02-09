ca6a55717ad892777e42104c1285120efb62038a
otto@Ubuntu20_04 <otto.lin@patere.net>
2022-02-09 21:20:01 +0800

    fail firebase read document

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
+
 import 'package:web_practice/screens/main_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -18,7 +24,34 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         primarySwatch: Colors.green,
       ),
-      home: MainPage(),
+      home: GetInfo(),
     );
+  }
+}
+
+class GetInfo extends StatelessWidget {
+  const GetInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('diaries').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Something went wrong'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator.adaptive());
+          }
+          return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot doc) {
+            // Map<String, dynamic> data =
+            //     document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(doc.get('display_name')),
+              subtitle: Text(doc.get('profession')),
+            );
+          }).toList());
+        });
   }
 }
