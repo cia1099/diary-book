c504594cee5ddbc2141dcf78531fc3188b24c1e4
otto@Ubuntu20_04 <otto.lin@patere.net>
2022-02-14 10:34:49 +0800

    runtime update onTap AlertDialog to listening diary data

--- lib/widgets/inner_list_card.dart
+++ lib/widgets/inner_list_card.dart
@@ -100,107 +100,135 @@ class InnerListCard extends StatelessWidget {
           ),
           onTap: () => showDialog(
             context: context,
-            builder: (context) => AlertDialog(
-              title: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: Row(
-                      children: [
-                        Text(
-                          formatDateFromTimestamp(diary.entryTime),
-                          style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontSize: 19,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Spacer(),
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) => UpdateEntryDialog(
-                                diary: diary,
-                                titleTextController: _titleTextController,
-                                descriptionTextController:
-                                    _descriptionTextController,
-                                linkReference: bookCollectionReference,
-                              ),
-                            );
-                            // Navigator.of(context).pop();
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete_forever_rounded),
-                          onPressed: () async {
-                            final isDeleted = await showDialog(
-                              context: context,
-                              builder: (context) => DeleteEntryDialog(
-                                  bookCollectionReference:
-                                      bookCollectionReference,
-                                  diary: diary),
-                            );
-                            if (isDeleted) Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              content: ListTile(
-                subtitle: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '•' + fromDateFromTimestampHour(diary.entryTime),
-                          style: TextStyle(color: Colors.green),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      height: MediaQuery.of(context).size.height * 0.5,
-                      child: Image.network(diary.photoUrls.isEmpty
-                          ? 'https://picsum.photos/400/200'
-                          : diary.photoUrls),
-                    ),
-                    Row(
-                      children: [
-                        Flexible(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+            builder: (context) => StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('diaries').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator.adaptive());
+                }
+                Diary? listenDiary;
+                try {
+                  listenDiary = snapshot.data!.docs
+                      .map((doc) => Diary.fromDocument(doc))
+                      .firstWhere((item) => item.id == diary.id);
+                } on StateError catch (_) {
+                  return AlertDialog(
+                    content: const Text('There is not this date.'),
+                    actions: [
+                      TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text('Close'))
+                    ],
+                  );
+                }
+                return AlertDialog(
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        child: Row(
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                diary.title,
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
+                            Text(
+                              formatDateFromTimestamp(listenDiary.entryTime),
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                diary.entry,
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
+                            Spacer(),
+                            IconButton(
+                              icon: Icon(Icons.edit),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => UpdateEntryDialog(
+                                    diary: listenDiary!,
+                                    titleTextController: _titleTextController,
+                                    descriptionTextController:
+                                        _descriptionTextController,
+                                    linkReference: bookCollectionReference,
+                                  ),
+                                );
+                                // Navigator.of(context).pop();
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete_forever_rounded),
+                              onPressed: () async {
+                                final isDeleted = await showDialog(
+                                  context: context,
+                                  builder: (context) => DeleteEntryDialog(
+                                      bookCollectionReference:
+                                          bookCollectionReference,
+                                      diary: listenDiary!),
+                                );
+                                if (isDeleted) Navigator.of(context).pop();
+                              },
                             ),
                           ],
-                        ))
+                        ),
+                      )
+                    ],
+                  ),
+                  content: ListTile(
+                    subtitle: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '•' +
+                                  fromDateFromTimestampHour(
+                                      listenDiary.entryTime),
+                              style: TextStyle(color: Colors.green),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          child: Image.network(listenDiary.photoUrls.isEmpty
+                              ? 'https://picsum.photos/400/200'
+                              : listenDiary.photoUrls),
+                        ),
+                        Row(
+                          children: [
+                            Flexible(
+                                child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    listenDiary.title,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    listenDiary.entry,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ))
+                          ],
+                        )
                       ],
-                    )
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text('Close'))
                   ],
-                ),
-              ),
-              actions: [
-                TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: Text('Cancel'))
-              ],
+                );
+              },
             ),
           ),
         ),
